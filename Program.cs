a4abfd9ed4d2a0729935b4d8f8a6ee863493b23b
Ashu4999 <ashutoshgorke77@gmail.com>
2024-09-01 03:26:19 +0530

    inbuild datastucute complete

--- Program.cs
+++ Program.cs
@@ -4,67 +4,42 @@
     {
         static void Main(string[] args)
         {
-            //// 1 byte is made up of 8 bits 00000000 - these bits can be used to store a number as follows
-            // //// Each bit can be worth 0 or 1 of the value it is placed in
-            // ////// From the right we start with a value of 1 and double for each digit to the left
-            // //// 00000000 = 0
-            // //// 00000001 = 1
-            // //// 00000010 = 2
-            // //// 00000011 = 3
-            // //// 00000100 = 4
-            // //// 00000101 = 5
-            // //// 00000110 = 6
-            // //// 00000111 = 7
-            // //// 00001000 = 8
+            string[] myGrocery = new string[2];
 
-            // 1 byte (8 bit) unsigned, where signed means it can be negative
-            byte myByte = 255;
-            byte mySecondByte = 0;
+            myGrocery[0] = "Apple";
+            myGrocery[1] = "Ice-Cream";
 
-            // 1 byte (8 bit) signed, where signed means it can be negative
-            sbyte mySbyte = 127;
-            sbyte mySecondSbyte = -128;
+            Console.WriteLine(myGrocery[0]);
+            Console.WriteLine(myGrocery[1]);
 
+            string[] myGrocery2 = { "Mango", "Choclate" };
 
-            // 2 byte (16 bit) unsigned, where signed means it can be negative
-            ushort myUshort = 65535;
+            Console.WriteLine(myGrocery2[0]);
+            Console.WriteLine(myGrocery2[1]);
 
-            // 2 byte (16 bit) signed, where signed means it can be negative
-            short myShort = -32768;
+            List<string> myGroceryList = new List<string> { "Milk", "Cheese" };
+            Console.WriteLine(myGroceryList[0]);
+            Console.WriteLine(myGroceryList[1]);
 
-            // 4 byte (32 bit) signed, where signed means it can be negative
-            int myInt = 2147483647;
-            int mySecondInt = -2147483648;
+            myGroceryList.Add("Oranges");
+            Console.WriteLine(myGroceryList[2]);
 
-            // 8 byte (64 bit) signed, where signed means it can be negative
-            long myLong = -9223372036854775808;
+            IEnumerable<string> myGroceryIEnumerable = myGroceryList;
+            Console.WriteLine(myGroceryIEnumerable.First());
 
+            string[,] my2DArray = new string[,] {
+                { "Mango", "Choclate" },
+                { "Milk", "Cheese" }
+            };
 
-            // 4 byte (32 bit) floating point number
-            float myFloat = 0.751f;
-            float mySecondFloat = 0.75f;
+            Console.WriteLine(my2DArray[0, 1]);
 
-            // 8 byte (64 bit) floating point number
-            double myDouble = 0.751;
-            double mySecondDouble = 0.75d;
+            Dictionary<string, string[]> myGroceryDict = new Dictionary<string, string[]> {
+                { "Dairy", new string[] { "Milk", "Cheese" }},
+                { "Market", new string[] { "Choclate", "Mango", "Oranges" }}
+            };
 
-            // 16 byte (128 bit) floating point number
-            decimal myDecimal = 0.751m;
-            decimal mySecondDecimal = 0.75m;
-
-            Console.WriteLine(myFloat - mySecondFloat);
-            Console.WriteLine(myDouble - mySecondDouble);
-            Console.WriteLine(myDecimal - mySecondDecimal);
-
-
-            //single quote for char and double for string
-            char singleChar = 'H';
-            string myString = "Hello World";
-            Console.WriteLine(myString);
-            string myStringWithSymbols = "!@#$@^$%%^&(&%^*__)+%^@##$!@%123589071340698ughedfaoig137";
-            Console.WriteLine(myStringWithSymbols);
-
-            bool myBool = true;
+            Console.WriteLine(myGroceryDict["Market"][0]);
         }
     }
 }
