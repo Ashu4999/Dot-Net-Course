c1bb5293befaa4146156f9be2ab8a8a0d761a8ff
Ashu4999 <ashutoshgorke77@gmail.com>
2024-09-01 02:44:44 +0530

    variables

--- Program.cs
+++ Program.cs
@@ -4,17 +4,67 @@
     {
         static void Main(string[] args)
         {
-            if (args.Length > 0) {
-                for(int i = 0; i < args.Length; i++) {
-                    Console.WriteLine($"args index {i}: {args[i]}");
-                }
-            }
-            // Write and WriteLine used to print but WriteLine append new line at the end.
-            Console.WriteLine("Line 1 With Break Line"); // New Line
-            Console.Write("Line 2");
-            Console.Write("Line 3");
-            Console.WriteLine("Line 4"); //New Line
-            Console.WriteLine("Line 5");
+            //// 1 byte is made up of 8 bits 00000000 - these bits can be used to store a number as follows
+            // //// Each bit can be worth 0 or 1 of the value it is placed in
+            // ////// From the right we start with a value of 1 and double for each digit to the left
+            // //// 00000000 = 0
+            // //// 00000001 = 1
+            // //// 00000010 = 2
+            // //// 00000011 = 3
+            // //// 00000100 = 4
+            // //// 00000101 = 5
+            // //// 00000110 = 6
+            // //// 00000111 = 7
+            // //// 00001000 = 8
+
+            // 1 byte (8 bit) unsigned, where signed means it can be negative
+            byte myByte = 255;
+            byte mySecondByte = 0;
+
+            // 1 byte (8 bit) signed, where signed means it can be negative
+            sbyte mySbyte = 127;
+            sbyte mySecondSbyte = -128;
+
+
+            // 2 byte (16 bit) unsigned, where signed means it can be negative
+            ushort myUshort = 65535;
+
+            // 2 byte (16 bit) signed, where signed means it can be negative
+            short myShort = -32768;
+
+            // 4 byte (32 bit) signed, where signed means it can be negative
+            int myInt = 2147483647;
+            int mySecondInt = -2147483648;
+
+            // 8 byte (64 bit) signed, where signed means it can be negative
+            long myLong = -9223372036854775808;
+
+
+            // 4 byte (32 bit) floating point number
+            float myFloat = 0.751f;
+            float mySecondFloat = 0.75f;
+
+            // 8 byte (64 bit) floating point number
+            double myDouble = 0.751;
+            double mySecondDouble = 0.75d;
+
+            // 16 byte (128 bit) floating point number
+            decimal myDecimal = 0.751m;
+            decimal mySecondDecimal = 0.75m;
+
+            Console.WriteLine(myFloat - mySecondFloat);
+            Console.WriteLine(myDouble - mySecondDouble);
+            Console.WriteLine(myDecimal - mySecondDecimal);
+
+
+            //single quote for char and double for string
+            char singleChar = 'H';
+            string myString = "Hello World";
+            Console.WriteLine(myString);
+            string myStringWithSymbols = "!@#$@^$%%^&(&%^*__)+%^@##$!@%123589071340698ughedfaoig137";
+            Console.WriteLine(myStringWithSymbols);
+
+            bool myBool = true;
         }
     }
 }
