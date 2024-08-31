81bf9ab3cba89bcec755b87952f6a8c6e2dc0e28
Ashu4999 <ashutoshgorke77@gmail.com>
2024-09-01 04:44:53 +0530

    control statements

--- Program.cs
+++ Program.cs
@@ -7,66 +7,31 @@
             int myInt = 5;
             int mySecondInt = 10;
 
-            Console.WriteLine(myInt.Equals(mySecondInt));
-            Console.WriteLine(myInt.Equals(mySecondInt / 2));
+            if (mySecondInt > myInt) {
+                Console.WriteLine("Second int is greater");
+            }
 
-            Console.WriteLine(myInt == mySecondInt);
-            Console.WriteLine(myInt == mySecondInt / 2);
+            string firstStr = "cow";
+            string secondStr = "cow";
 
-            Console.WriteLine(myInt != mySecondInt);
-            Console.WriteLine(myInt != mySecondInt / 2);
+            if (firstStr == secondStr) {
+                Console.WriteLine("Equal");
+            } else if (firstStr.ToLower() == secondStr.ToLower()) {
+                Console.WriteLine("Equal without case sensitivity");
+            } else {
+                Console.WriteLine("Not equal");
+            }
 
-            Console.WriteLine(myInt >= mySecondInt);
-            Console.WriteLine(myInt > mySecondInt);
-            Console.WriteLine(myInt <= mySecondInt);
-            Console.WriteLine(myInt < mySecondInt);
+            string option = "one";
 
-            Console.WriteLine(myInt);
-            myInt++;
-            Console.WriteLine(myInt);
-
-            myInt += 7;
-            Console.WriteLine(myInt);
-
-            myInt -= 8;
-            Console.WriteLine(myInt);
-
-            Console.WriteLine(myInt * mySecondInt);
-            Console.WriteLine(myInt / mySecondInt);
-            Console.WriteLine(myInt + mySecondInt);
-            Console.WriteLine(myInt - mySecondInt);
-
-            //PEMDAS ORDER
-            Console.WriteLine(5 + 5 * 10);
-            Console.WriteLine((5 + 5) * 10);
-
-            Console.WriteLine($"Pow of 5: {Math.Pow(5, 2)}");
-            Console.WriteLine($"Sqrt of 25: {Math.Sqrt(25)}");
-
-            string str = "\"first ";
-            Console.WriteLine(str);
-
-            str += "second ";
-            Console.WriteLine(str);
-
-            str = str + "third\"";
-            Console.WriteLine(str);
-
-            string[] splitArray = str.Split(" ");
-            Console.WriteLine(splitArray[0]);
-            Console.WriteLine(splitArray[1]);
-            Console.WriteLine(splitArray[2]);
-
-
-            Console.WriteLine(true && true);
-            Console.WriteLine(true && false);
-            Console.WriteLine(false && true);
-            Console.WriteLine(false && false);
-
-            Console.WriteLine(true || true);
-            Console.WriteLine(true || false);
-            Console.WriteLine(false || true);
-            Console.WriteLine(false || false);
+            switch(option) {
+                case "one": Console.WriteLine("One");
+                            break;
+                case "two": Console.WriteLine("Two");
+                            break;     
+                default   : Console.WriteLine("Invalid Option");
+                            break;      
+            }
         }
     }
 }
