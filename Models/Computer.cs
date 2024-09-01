c62d6686f758fff245174f38be7a437c5b3b179e
Ashu4999 <ashutoshgorke77@gmail.com>
2024-09-01 17:59:54 +0530

    handling json serialize and deserialize

--- Models/Computer.cs
+++ Models/Computer.cs
@@ -5,11 +5,11 @@ namespace HelloWorld.Models
         // private string _motherboard;
         // private string MotherBoard { get { return _motherboard; } set { _motherboard = value; } }
         public int ComputerId { get; set; }
-        public string MotherBoard { get; set; } = String.Empty;
+        public string Motherboard { get; set; } = String.Empty;
         public int? CPUCores { get; set; } = 0;
         public bool HasWifi { get; set; }
         public bool HasLTE { get; set; }
-        public DateTime ReleaseDate { get; set; }
+        public DateTime? ReleaseDate { get; set; }
         public decimal Price { get; set; }
         public string VideoCard { get; set; } = String.Empty;
 
--- Program.cs
+++ Program.cs
@@ -1,9 +1,12 @@
 ﻿using System.Data;
+using System.Text.Json;
 using Dapper;
 using HelloWorld.Data;
 using HelloWorld.Models;
 using Microsoft.Data.SqlClient;
 using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 
 namespace HelloWorld
 {
@@ -11,11 +14,11 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            //     IConfiguration config = new ConfigurationBuilder()
-            //         .AddJsonFile("appsettings.json")
-            //         .Build();
+            IConfiguration config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
 
-            //     DataContextEF dataContextEF = new DataContextEF(config);
+            DataContextEF dataContextEF = new DataContextEF(config);
             //     Computer myComputer = new Computer() {
             //         MotherBoard = "Z690",
             //         HasWifi = true,
@@ -37,15 +40,59 @@ namespace HelloWorld
             //         Console.WriteLine($"'{currComputer.ComputerId}','{currComputer.MotherBoard}','{currComputer.CPUCores}','{currComputer.HasWifi}','{currComputer.HasLTE}','{currComputer.ReleaseDate}','{currComputer.Price}','{currComputer.VideoCard}'");
             //     }
 
-            string writeContent = "HERE is the first line\nThen second\nSo third\n";
-            // File.WriteAllText("log.txt", writeContent);
+            // string writeContent = "HERE is the first line\nThen second\nSo third\n";
+            // // File.WriteAllText("log.txt", writeContent);
 
-            using StreamWriter openFile = new("log.txt", append: true);
-            openFile.WriteLine(writeContent);
-            openFile.Close();
+            // using StreamWriter openFile = new("log.txt", append: true);
+            // openFile.WriteLine(writeContent);
+            // openFile.Close();
 
-            string fileContent = File.ReadAllText("log.txt");
-            Console.WriteLine(fileContent);
+            // string fileContent = File.ReadAllText("log.txt");
+            // Console.WriteLine(fileContent);
+
+            string computersJson = File.ReadAllText("Computers.json");
+
+            JsonSerializerOptions options = new JsonSerializerOptions()
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+            };
+
+            //System.Text.Json.JsonSerializer - inbuild 
+            //JsonConvert - (Newtonsoft.Json pkg)
+            IEnumerable<Computer>? computerSystem = System.Text.Json.JsonSerializer.Deserialize<IEnumerable<Computer>>(computersJson, options);
+
+            IEnumerable<Computer>? computerNewtonSoft = JsonConvert.DeserializeObject<IEnumerable<Computer>>(computersJson);
+
+            if (computerNewtonSoft != null)
+            {
+                foreach (Computer currComputer in computerNewtonSoft)
+                {
+                    Computer computerObject = new Computer()
+                    {
+                        Motherboard = currComputer.Motherboard,
+                        CPUCores = currComputer.CPUCores,
+                        HasWifi = currComputer.HasWifi,
+                        HasLTE = currComputer.HasLTE,
+                        ReleaseDate = currComputer.ReleaseDate,
+                        Price = currComputer.Price,
+                        VideoCard = currComputer.VideoCard
+                    };
+
+                    dataContextEF.Computer.Add(computerObject);
+                }
+                dataContextEF.SaveChanges();
+            }
+
+            JsonSerializerSettings settings = new JsonSerializerSettings()
+            {
+                ContractResolver = new CamelCasePropertyNamesContractResolver()
+            };
+
+            string computerCopyNewtonSoft = JsonConvert.SerializeObject(computerNewtonSoft, settings);
+            File.WriteAllText("computerCopyNewtonSoft.txt", computerCopyNewtonSoft);
+
+            string computerCopySystem = System.Text.Json.JsonSerializer.Serialize(computerNewtonSoft, options);
+            File.WriteAllText("computerCopySystem.txt", computerCopySystem);
         }
     }
 }
